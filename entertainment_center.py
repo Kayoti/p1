abcfe241c795687bfd2f4f55e4585a8c4c701cc9
Wael <ymnwael@gmail.com>
2015-10-04 17:20:23 -0600

    refactor: cleaning directory after merge docs:Added Info class documentation

--- entertainment_center.py
+++
@@ -1,7 +0,0 @@
-#import modules
-import media
-import fresh_tomatoes
-
-#create instances of movie data
-#store instances in an array
-# use array to output data using fresh_tomatoes module
--- p1/models.py
+++ p1/models.py
@@ -2,7 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Info(models.Model):
-    
+    """A class to process movie related data
+
+    This class Implementsmodels from django db to store data related to a specific movie
+    In this class information such as title image trailer url
+    are instantiated and stored in DB for text display and video playback purposes
+
+    Attributes:
+        title: Object to store A String  to display movie title.
+        link: Object to store A String  contains url for an image.
+        imgurl: Object to store A String contains url for a video for playback
+    """
     title= models.CharField(max_length=400)
     link=models.CharField(max_length=400)
     imgurl=models.CharField(max_length=400)
